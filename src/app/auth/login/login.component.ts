c507da56bb98368a1fd37077bdd81140f4668c7b
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-12-02 23:14:58 +0000

    another commit

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
-
-
+import { CommonModule } from '@angular/common';
+import { Component  , OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink , ReactiveFormsModule , CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   type: string = 'password';
+
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+
+  ngOnInit(): void { 
+    console.log(this.loginForm.controls)
+  }
+   
+ 
 
   togglePassword() {
     if (this.type === 'password') {
@@ -17,6 +29,16 @@ export class LoginComponent {
     } else {
       this.type = 'password'; 
     }
+  }
+
+
+  get Fields() {
+    return this.loginForm.controls; 
+  }
+
+  submitLoginForm() {
+    const values  = this.loginForm.value; 
+    console.log(values); 
   }
 
 }

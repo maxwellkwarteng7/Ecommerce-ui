fcbeded61707669b744bf52bb6e548d2ed9e9cb1
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-12-09 23:04:56 +0000

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
+import { PasscodeComponent } from './passcode/passcode.component';
 
 export const routes: Routes = [
     {
@@ -20,9 +21,13 @@ export const routes: Routes = [
     {
         path: 'sign-up', 
         component : SignUpComponent
-    } , 
+    }, 
     {
-        path : 'forgot-password' , 
-    
+        path: "forgot-password", 
+        component : PasscodeComponent
+    }, 
+    {
+        path: "verify-email", 
+        component : PasscodeComponent
     }
 ];

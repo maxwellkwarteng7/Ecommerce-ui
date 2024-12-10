351822bd098c8bd70daaa70e3f41f5e5da69125a
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2024-12-10 17:08:31 +0000

    changes -m

--- src/app/passcode/passcode.component.ts
+++ src/app/passcode/passcode.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-passcode',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule  , ReactiveFormsModule],
   templateUrl: './passcode.component.html',
   styleUrl: './passcode.component.scss'
 })
-export class PasscodeComponent {
+export class PasscodeComponent implements OnInit {
+
+
+  currentRoute!: string;  
+
+  constructor(private router  : ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+   this.currentRoute = this.router.snapshot.routeConfig?.path || ''; 
+  }
+
+  forgotPasswordForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
+
+  get forgotPasswordFields() {
+    return this.forgotPasswordForm.controls; 
+  }
 
 }

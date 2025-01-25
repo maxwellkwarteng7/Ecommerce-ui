d2e8e362f89e7536ac98dd114c61d4c6ae8acb7a
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2025-01-25 23:12:51 +0000

    commitment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from "./home/home.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

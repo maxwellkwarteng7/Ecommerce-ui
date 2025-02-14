bd90586d1423abf9dd9ce654cf67ff442e921cc2
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2025-02-14 16:03:04 +0000

    changes

--- src/app/models/productTemplate.ts
+++ src/app/models/productTemplate.ts
@@ -1,6 +1,18 @@
 
 export interface Category {
-    id : number , 
-    name: string, 
+    id : number ,
+    name: string,
     image : string
+}
+
+export interface Product {
+  id : number ,
+  name : string ,
+  image : string ,
+  price : number ,
+  discountPrice : number ,
+  categoryId : number ,
+  tagId : number ,
+  stock : number ,
+  description : string
 }
--- src/app/services/product-service/product-service.service.ts
+++ src/app/services/product-service/product-service.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Category } from '../../models/productTemplate';
+import { Category , Product } from '../../models/productTemplate';
 import { environment } from '../../../environments/environment.development';
 import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,12 @@ import { Observable } from 'rxjs';
 export class ProductServiceService {
 
   constructor(private http: HttpClient) { }
-  
+
   getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(`${environment.baseUrl}/category`);
+  }
+
+  getProductByTag(tag : string) : Observable<Product[]>{
+    return this.http.get<Product[]>(`${baseUrl}/tag/products?tag=${tag}`);
   }
 }

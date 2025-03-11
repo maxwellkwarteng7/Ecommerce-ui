8c7fc589e0b3997f924ac3b4a3e9898ac5a60c59
Maxwell  Kwarteng <maxwellkwarteng7@gmail.com>
2025-03-11 15:46:00 +0000

    flow almost done

--- src/app/payment-success/payment-success.component.ts
+++ src/app/payment-success/payment-success.component.ts
@@ -1,57 +1,62 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { PaymentService } from '../services/payment-service/payment.service';
-import { ToastrService } from 'ngx-toastr';
-import { AlertServiceService } from '../services/sweetAlert/alert-service.service';
-import { CartServiceService } from '../services/cart-service/cart-service.service';
+import { Component, inject, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { PaymentService } from "../services/payment-service/payment.service";
+import { ToastrService } from "ngx-toastr";
+import { AlertServiceService } from "../services/sweetAlert/alert-service.service";
+import { CartServiceService } from "../services/cart-service/cart-service.service";
 
 @Component({
-  selector: 'app-payment-success',
+  selector: "app-payment-success",
   standalone: true,
   imports: [],
-  templateUrl: './payment-success.component.html',
-  styleUrl: './payment-success.component.scss'
+  templateUrl: "./payment-success.component.html",
+  styleUrl: "./payment-success.component.scss",
 })
 export class PaymentSuccessComponent implements OnInit {
-
-  // injections 
-  activeRoute = inject(ActivatedRoute); 
-  paymentService = inject(PaymentService); 
-  toaster = inject(ToastrService); 
-  sweetAlert = inject(AlertServiceService); 
-  router = inject(Router); 
-  cartService = inject(CartServiceService); 
+  // injections
+  activeRoute = inject(ActivatedRoute);
+  paymentService = inject(PaymentService);
+  toaster = inject(ToastrService);
+  sweetAlert = inject(AlertServiceService);
+  router = inject(Router);
+  cartService = inject(CartServiceService);
+  isLoading = true;
 
   ngOnInit(): void {
-    const addressId: number = parseInt(localStorage.getItem('addressId') || '');
-     
-      this.activeRoute.queryParams.subscribe(params => {
-        const reference = params['reference'];  
-        if (reference) {
-          this.paymentService.verifyPaystackPayment(reference , addressId).subscribe({
-            next: () => {
-              this.sweetAlert.successMessage('Payment Successful', ''); 
-              this.clearUserCart();
-            }, 
-            error: (error) => {
-              console.log(error); 
-              this.router.navigate(['/shipping']); 
-              this.toaster.error('Error verifying payment'); 
-              
-            }
-          })
-        }
-      })
-  }
+    const addressId: number = parseInt(localStorage.getItem("addressId") || "");
 
+    this.activeRoute.queryParams.subscribe((params) => {
+      const reference = params["reference"];
+      if (reference) {
+        this.paymentService
+          .verifyPaystackPayment(reference, addressId)
+          .subscribe({
+            next: () => {
+              this.sweetAlert.successMessage("Payment Successful", "");
+              this.clearUserCart();
+            },
+            error: (error) => {
+              console.log(error);
+              this.router.navigate(["/shipping"]);
+              this.toaster.error("Error verifying payment");
+            },
+          });
+      }
+    });
+  }
 
   clearUserCart() {
     this.cartService.clearUserCart().subscribe({
-      next: () => this.router.navigate(['/orders']), 
+      next: () => {
+        localStorage.setItem("userCart", "[]");
+        this.isLoading = false; 
+        this.router.navigate(["/orders"]);
+      },
       error: (error) => {
-        console.log(error); 
-        this.toaster.error('Error clearing user cart'); 
-      } 
-    })
+        console.log(error);
+        this.isLoading = false;
+        this.toaster.error("Error clearing user cart");
+      },
+    });
   }
 }
